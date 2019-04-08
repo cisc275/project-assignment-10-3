0b165fda59c6d3879ce1bbcdd5a9d1cc982bb891
Letian Zhang <zltian@udel.edu>
2019-04-08 16:26:48 -0400

    Map

--- src/Map.java
+++ src/Map.java
@@ -1,4 +1,25 @@
 
 public class Map {
-
+	private int width;
+	private int length;
+	private int x;//for current location on map 
+	private int y;
+	private int status;// 0-100%
+	
+	public Map() {
+		
+	}
+	
+	
+	/*
+	 * Parameter: 
+	 * 			int status: the percentage of completeness
+	 * 
+	 * Description: this function calculate the current location based on the status;
+	 */
+	public void updateLocation(int status) {
+		
+	}
+	
+	
 }
