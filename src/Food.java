3ae1faeecd7fae5f7a8eaef541339ab6af047f7a
Letian Zhang <zltian@udel.edu>
2019-04-08 13:21:22 -0400

    Items

--- src/Food.java
+++ src/Food.java
@@ -1,4 +1,21 @@
 
-public class Food {
+public class Food extends Items{
+
+	public Food(int width, int length) {
+		super(width, length);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean beHit() {
+		
+		return false;
+	}
+
+	@Override
+	public void exit() {
+		
+		
+	}
 
 }
--- src/PowerUp.java
+++ src/PowerUp.java
@@ -1,4 +1,21 @@
 
-public class PowerUp {
+public class PowerUp extends Items{
+
+	public PowerUp(int width, int length) {
+		super(width, length);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean beHit() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void exit() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
