56a99b9ee54899a506d47a2b2aa62d02f9134b83
Letian Zhang <zltian@udel.edu>
2019-04-08 15:53:17 -0400

    Model

--- src/Model.java
+++ src/Model.java
@@ -1,4 +1,25 @@
 
-public class Model {
 
+public class Model {
+	
+	private ClapperRail clapperrail;
+	private RedKnot redknot;
+	
+	private int width;
+    private int height;
+
+    
+	/*
+	 * Constructor
+	 */
+	public Model() {
+		
+	}
+	
+	public void updateLocation() {
+		
+	}
+	
+	
+	
 }
