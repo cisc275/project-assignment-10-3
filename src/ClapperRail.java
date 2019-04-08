27da6750129da8435c681712464039ebc148866b
Letian Zhang <zltian@udel.edu>
2019-04-08 15:38:23 -0400

    birds

--- src/ClapperRail.java
+++ src/ClapperRail.java
@@ -1,4 +1,17 @@
 
 public class ClapperRail {
-
+	private int x;
+	private int y;
+	private int length;
+	private int width;
+	private int xVel;
+	private int yVel;
+	
+	/*
+	 * Constructor
+	 */
+	public ClapperRail() {
+		
+	}
+	
 }
--- src/RedKnot.java
+++ src/RedKnot.java
@@ -1,4 +1,18 @@
 
 public class RedKnot {
+	private int x;
+	private int y;
+	private int length;
+	private int width;
+	private int xVel;
+	private int yVel;
+	
+	
+	/*
+	 * Constructor 
+	 */
+	public RedKnot() {
+		
+	}
 
 }
