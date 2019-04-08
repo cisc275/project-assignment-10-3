6cad97214a7f28edd8fd9f57e4e8f7a568be0b43
Letian Zhang <zltian@udel.edu>
2019-04-08 13:04:42 -0400

    Obstacle

--- src/Obstacle.java
+++ src/Obstacle.java
@@ -1,4 +1,30 @@
 
 public class Obstacle {
-
+	private int width;
+	private int length;
+	
+	
+	
+	
+	
+	/*
+	 * Return: Boolean
+	 * Description: This method detects if the bird is get hit or not by the obstacle
+	 * 				returns TRUE if hits,
+	 * 						else FALSE
+	 * 	
+	 */
+	public boolean beHit() {
+		return false;
+	}
+	
+	
+	/*
+	 * Description: THe method will be called if beHit == TRUE;
+	 * 
+	 * Effects: Obstacles dispear from the screen
+	 */
+	public void exit() {
+		
+	}
 }
