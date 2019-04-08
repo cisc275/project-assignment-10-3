b028eb164c6f44c2a6c91a3dd5b56a57c4eda515
Letian Zhang <zltian@udel.edu>
2019-04-08 16:48:07 -0400

    View

--- src/View.java
+++ src/View.java
@@ -1,4 +1,62 @@
+import java.awt.Graphics;
 
-public class View {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class View extends JPanel{
+	private final int frameWidth = 0;// TBD
+    private final int frameHeight = 0;
+    
+    private ClapperRail clapperRail;
+    private RedKnot redKnot;
+    
+    JFrame frame;
+    JButton button_clapperrail;
+    JButton button_redknote;
+    
+    public View() {
+    	
+    }
+    
+    public void paintComponent(Graphics g) {
+    	
+    }
+    
+    //repaint();
+    //update birds location, items location, and score(status)
+    public void update() {
+    	
+    }
+    
+    //
+    //Create Images 
+    //
+    
+    public void createBirdsImage() {
+    	
+    }
+    
+    public void createPowerUpImage() {
+    	
+    }
+    
+    public void createFoodClapperRail_Image() {
+    	
+    }
+    
+    public void createFoodRedKnot_Image() {
+    	
+    }
+    
+    public void createObstacleClapperRailImage() {
+    	
+    }
+    public void createObstacleRedKnotImage() {
+    	
+    }
+    
+    
+    
 
 }
