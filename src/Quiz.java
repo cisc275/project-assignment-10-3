2c8459b25db72821914c99ef5b11839a48f6f48f
mlyons1414 <36232436+mlyons1414@users.noreply.github.com>
2019-04-08 13:29:27 -0400

    Quiz

--- src/Quiz.java
+++ src/Quiz.java
@@ -1,4 +1,27 @@
+import java.util.*;
 
 public class Quiz {
-
+	/**
+	 * stores the index of the correct answer
+	 */	
+	private int correctAnswer;
+	
+	/**
+	 * question that the player needs to answer
+	 */
+	private String question;
+	
+	/**
+	 * list of potential answers to the multiple choice question
+	 */
+	private List<String>answers;
+	
+	/**
+	 * Checks whether the question has been answered correctly
+	 * 
+	 * Returns true if correct, false if incorrect
+	 */
+	boolean checkCorrect() {
+		return false;
+		}
 }
